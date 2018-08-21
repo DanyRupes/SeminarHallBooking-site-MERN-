7f228c08d626acb3d5490237ab7b9b016edd0ee9
Danyrupes <danyrupes.1505026@srec.ac.in>
2018-08-22 02:06:23 +0530

    site project started soon

--- server.js
+++ server.js
@@ -1,9 +1,12 @@
 var express = require('express')
+var path = require('path')
 var app = express()
 
-app.use('./views')
+app.use(express.static(path.join(__dirname,'/views')))
 
 app.get('/', function(req, res){
 
     res.sendFile(__dirname,'/index.html')
 })
+
+app.listen(8000,console.log('http://localhost:8000'))
